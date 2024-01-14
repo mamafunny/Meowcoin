9164b3557aabdc9d7ac0774b2f840894f9bcedae
ZachPrice <zach.price105@gmail.com>
2024-01-14 16:56:40 -0600

    another test for OSX building

--- .github/scripts/00-install-deps.sh
+++ .github/scripts/00-install-deps.sh
@@ -65,6 +65,7 @@ elif [[ ${OS} == "osx" ]]; then
     s3curl \
     sleuthkit \
     bison \
+    libstdc++-12-dev \
     libtinfo5
     
 elif [[ ${OS} == "linux" || ${OS} == "linux-disable-wallet" || ${OS} == "aarch64" || ${OS} == "aarch64-disable-wallet" ]]; then
